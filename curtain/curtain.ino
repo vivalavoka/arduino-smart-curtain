99f57408d33cb9bca0f2c159b11a794b3bc3fad6
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-07-16 23:38:16 +0300

    Install stepper lib

--- curtain/curtain.ino
+++ curtain/curtain.ino
@@ -1,12 +1,51 @@
-#include <CyberLib.h>
+// #include <CyberLib.h>
 
-void setup()
-{
-  D12_Out;
+// void setup()
+// {
+//   D8_Out;
+//   D9_Out;
+//   D10_Out;
+//   D11_Out;
+// }
+
+// void loop()
+// {
+//   D8_High;
+//   D9_High;
+//   D10_Low;
+//   D11_Low;
+//   delay_ms(2000);
+//   // D8_Low;
+//   // D9_Low;
+//   // D10_Low;
+//   // D11_Low;
+//   // delay_ms(2000);
+// }
+
+#include <Stepper.h>
+
+// указываем количество шагов нашего мотора - в нашем случае 64
+#define STEPS 64
+
+// Создаем экземпляр класса Stepper, и указываем 
+// количество шагов и пины подключния
+Stepper stepper(STEPS, 8, 9, 10, 11);
+
+// переменная для запоминания предыдущего значения
+int previous = 0;
+
+void setup() {
+  // Устанавливаем скорость вращения в об/мин
+  stepper.setSpeed(90);
 }
 
-void loop()
-{ 
- D12_Inv;
- delay_ms(1000);
+void loop() {
+  // новое значение полученное с датчика
+  int val = analogRead(0); // При подключении датчика, 0 заменить на пин датчика
+
+  // Прокрутить на разницу шагов между новым полученным значением и предыдущим
+  stepper.step(val - previous);
+
+  // Запоминаем новое полученное значение
+  previous = val;
 }
