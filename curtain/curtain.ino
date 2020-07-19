e0f930fbcd42bc9d4f4dbdf20749ddd7c6766682
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-07-19 18:16:37 +0300

    Create state machine

--- curtain/curtain.ino
+++ curtain/curtain.ino
@@ -1,62 +1,132 @@
-// Видеообзоры и уроки работы с ARDUINO на YouTube-канале IOMOIO: https://www.youtube.com/channel/UCmNXABaTjX_iKH28TTJpiqA
+// Дребезг, случайные замыкания
+#define button_debounce 20
+// Клик
+#define button_hold 250
+// Долгое нажатие
+#define button_long 3000
+// Ошибка, на кнопку сели
+#define button_idle 5000
 
-#include <CustomStepper.h>            // Подключаем библиотеку управления шаговым двигателем. По умолчанию настроена на двигатель 28BYJ-48-5V
-CustomStepper stepper(8, 9, 10, 11);  // Указываем пины, к которым подключен драйвер шагового двигателя
+#include <CustomStepper.h>
 
-const int buttonPin = 2;
+// Указываем пины, к которым подключен драйвер шагового двигателя
+CustomStepper stepper(8, 9, 10, 11);
 
-int example = 1;
-int buttonState = 0;
-int touched = 0;
+// События
+enum event {Press, Release, WaitDebounce, WaitHold, WaitLongHold, WaitIdle};
 
-void setup()
-{
+enum buttonState {Idle, PreClick, Click, Hold, LongHold, ForcedIdle};
+
+enum buttonState btnState = Idle;
+
+enum motorState {RunCW, RunCCW, Stop};
+
+enum motorState mtrState = Stop;
+
+unsigned long pressTimestamp;
+
+const int btPin = 2;
+
+void setup() {
   stepper.setRPM(12);                 // Устанавливаем кол-во оборотов в минуту
   stepper.setSPR(4075.7728395);       // Устанавливаем кол-во шагов на полный оборот. Максимальное значение 4075.7728395
 
-  // pinMode(2, INPUT);
-  // Serial.begin(9600);
+  // Кнопка
+  pinMode(2, INPUT);
+
+  Serial.begin(9600);
 }
 
-void loop()
-{
-  // buttonState = digitalRead(buttonPin);
+void loop() {
+  unsigned long mls = millis();
 
-  // if (buttonState == HIGH && touched == 0) {
-  //   touched = 1;
-  //   Serial.print("Start\n");
-  //   stepper.setDirection(CW);
-  //   stepper.rotate();
-
-  // } else if (buttonState == LOW && touched == 1) {
-  //   touched = 0;
-
-  //   Serial.print("Stop\n");
-  //   stepper.setDirection(STOP);
-  // }
-
-  // stepper.run();
-
-  if (stepper.isDone() and example == 1)  // Когда предыдущая команда выполнена (см. ниже), метод stepper.isDone() возвращает true
-  {
-    Serial.print("Example 1\n");
-    stepper.setDirection(CW);         // Устанавливает направление вращения. Может принимать 3 значения: CW - по часовой, CCW - против часовой, STOP
-    stepper.rotate(1);                // Устанавливает вращение на заданное кол-во оборотов
-    example = 2;
+  if (digitalRead(btPin)) {
+    doEvent(Press);
+  } else {
+    doEvent(Release);
   }
-  if (stepper.isDone() and example == 2)
-  {
-    Serial.print("Example 2\n");
-    stepper.setDirection(CCW);
-    stepper.rotateDegrees(90);        // Поворачивает вал на заданное кол-во градусов. Можно указывать десятичную точность (например 90.5), но не принимаются отрицательные значения
-    example = 3;
+
+  if (mls - pressTimestamp > button_debounce) {
+    doEvent(WaitDebounce);
   }
-  if (stepper.isDone() and example == 3)
-  {
-    Serial.print("Example 3\n");
-    stepper.setDirection(CW);
+  if (mls - pressTimestamp > button_hold) {
+    doEvent(WaitHold);
+  }
+  if (mls - pressTimestamp > button_long) {
+    doEvent(WaitLongHold);
+  }
+  if (mls - pressTimestamp > button_idle) {
+    doEvent(WaitIdle);
+  }
+
+  if (mtrState == Stop) {
+    stepper.setDirection(STOP);
+  }
+
+  if (stepper.isDone()) {    
+    if (mtrState == RunCW) {
+      Serial.print("Rotate CW\n");
+      stepper.setDirection(CW);
+    } else if (mtrState == RunCCW) {
+      Serial.print("Rotate CCW\n");
+      stepper.setDirection(CCW);
+    } else if (mtrState == Stop) {
+      stepper.setDirection(STOP);
+    }
     stepper.rotate();
-    // Будет вращать пока не получит команду о смене направления или пока не получит директиву STOP
   }
-  stepper.run();                      // Этот метод обязателен в блоке loop. Он инициирует работу двигателя, когда это необходимо
+  stepper.run();
+}
+
+void doEvent(enum event e) {
+  switch (e) {
+    case Press:
+      if (btnState == Idle) {
+        btnState =  PreClick;
+        pressTimestamp = millis();
+      }
+      break;
+    case Release:
+      onClick(btnState);
+      btnState = Idle;
+      break;
+    case WaitDebounce:
+      if (btnState == PreClick) {
+        btnState = Click;
+      }
+      break;
+    case WaitHold:
+      if (btnState == Click) {
+        btnState = Hold;
+      }
+      break;
+    case WaitLongHold:
+      if (btnState == Hold) {
+        btnState = LongHold;
+      }
+      break;
+    case WaitIdle:
+      if (btnState == LongHold) {
+        // btnState = Idle;
+      }
+      break;
+  }
+}
+
+void onClick(enum buttonState s) {
+  switch (s) {
+    case Click:
+      mtrState = mtrState == RunCW ? Stop : RunCW;
+      Serial.print("Click\n");
+      break;
+    case Hold:
+      mtrState = Stop;
+      Serial.print("Hold\n");
+      break;
+    case LongHold:
+      Serial.print("LongHold\n");
+      break;
+    default:
+      break;
+  }
 }
